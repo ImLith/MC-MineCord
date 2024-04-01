81c8c45004159624d7cf0dac88048e83e71a0d6f
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-01 07:59:44 +0300

    fix: displaying unlocked recipes

--- src/main/java/com/lith/minecord/events/player/PlayerAchievement.java
+++ src/main/java/com/lith/minecord/events/player/PlayerAchievement.java
@@ -7,19 +7,38 @@ import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import com.lith.minecord.Static;
 import com.lith.minecord.config.ConfigManager;
 import com.lith.minecord.discord.DiscordManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import io.papermc.paper.advancement.AdvancementDisplay;
 
 public class PlayerAchievement implements Listener {
     @EventHandler
     public void onPlayerAchievement(PlayerAdvancementDoneEvent event) {
         Advancement advancement = event.getAdvancement();
 
+        AdvancementDisplay display = advancement.getDisplay();
+        if (display == null)
+            return;
+
+        if (!event.getPlayer().getAdvancementProgress(advancement).isDone())
+            return;
+
+        Component titleComponent = display.title();
+        String advancementTitle = titleComponent != null
+                ? PlainTextComponentSerializer.plainText().serialize(titleComponent)
+                : "";
+
+        Component descriptionComponent = display.description();
+        String advancementDescription = descriptionComponent != null
+                ? PlainTextComponentSerializer.plainText().serialize(descriptionComponent)
+                : "";
+
         DiscordManager.init().sendMessage(
                 ConfigManager.livechatConfig.channelId,
                 ConfigManager.livechatConfig.achievement
                         .replace(Static.MessageKey.PLAYER_NAME, event.getPlayer().getName())
-                        .replace(Static.MessageKey.ACHIEVEMENT_NAME, advancement.getKey().getKey())
-                        .replace(Static.MessageKey.ACHIEVEMENT_DESCRIPTION, advancement.toString())
+                        .replace(Static.MessageKey.ACHIEVEMENT_NAME, advancementTitle)
+                        .replace(Static.MessageKey.ACHIEVEMENT_DESCRIPTION, advancementDescription));
 
-        );
     }
 }
