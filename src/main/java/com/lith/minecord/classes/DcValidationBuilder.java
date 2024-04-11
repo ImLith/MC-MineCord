baa63380f9b1c3c4c9ba893bbe29020ec3abfcce
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-11 03:28:50 +0300

    feat: added message vallidation builder rules

---
+++ src/main/java/com/lith/minecord/classes/DcValidationBuilder.java
@@ -0,0 +1,10 @@
+package com.lith.minecord.classes;
+
+public class DcValidationBuilder {
+    public Boolean isOnlyFromGuild = false;
+    public Boolean isOnlyTextChannel = true;
+    public Boolean isOnlyUser = true;
+    public Boolean isOnlyTextMessage = true;
+    public Boolean allowReplyMessage = true;
+    public String requiredChannelId = null;
+}
